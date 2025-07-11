e1d609c683f5d0e1e15eed136173a00e2cdccdae
一纸忘忧 <i@ikxin.com>
2025-07-11 20:48:25 +0800

    feat: using arco-desigin and tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import ArcoVue from '@arco-design/web-vue'
 import App from './App.vue'
+import '@arco-design/web-vue/dist/arco.css'
+import './style.css'
 
-createApp(App).mount('#app')
+createApp(App).use(ArcoVue).mount('#app')
