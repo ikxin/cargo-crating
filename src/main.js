b187096dbbeab590f21249091abe7448e9d9211f
一纸忘忧 <i@ikxin.com>
2025-07-15 20:53:31 +0800

    feat: add default cargo and truck data

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import ArcoVue from '@arco-design/web-vue'
 import App from './App.vue'
 import '@arco-design/web-vue/dist/arco.css'
 import './style.css'
+import { useStorage } from '@vueuse/core'
+import { cargoDefaultData, truckDefaultData } from './utils/default'
 
 createApp(App).use(ArcoVue).mount('#app')
+
+useStorage('cargo-crating-database', {
+  cargo: cargoDefaultData,
+  truck: truckDefaultData,
+})
