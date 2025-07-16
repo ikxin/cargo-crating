9f31a2805f24ec7c0b1b0407ca231919ff1f2c13
一纸忘忧 <i@ikxin.com>
2025-07-16 13:50:48 +0800

    style: global window scrollbar

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,20 @@ import SpecTable from './components/SpecTable.vue'
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 p-4">
-    <ATabs default-active-key="2" type="capsule" size="large">
-      <ATabPane key="1" title="货车模式">
-        <TruckMode />
-      </ATabPane>
-      <ATabPane key="2" title="货物模式">
-        <CargoMode />
-      </ATabPane>
-    </ATabs>
-    <div class="flex gap-4">
-      <SpecTable class="w-3/7" prop="truck" name="货车" />
-      <SpecTable class="w-4/7" prop="cargo" name="货物" />
+  <AScrollbar class="h-screen w-screen overflow-auto">
+    <div class="flex flex-col gap-4 p-4">
+      <ATabs default-active-key="2" type="capsule" size="large">
+        <ATabPane key="1" title="货车模式">
+          <TruckMode />
+        </ATabPane>
+        <ATabPane key="2" title="货物模式">
+          <CargoMode />
+        </ATabPane>
+      </ATabs>
+      <div class="grid grid-cols-7 gap-4">
+        <SpecTable class="col-span-3" prop="truck" name="货车" />
+        <SpecTable class="col-span-4" prop="cargo" name="货物" />
+      </div>
     </div>
-  </div>
+  </AScrollbar>
 </template>
