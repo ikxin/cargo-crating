60b4108b8454a462b82ba277538a90f0cbfdacef
一纸忘忧 <i@ikxin.com>
2025-07-18 11:10:38 +0800

    feat: cargo and truck data with detail metrics

--- src/components/CratingDetail.vue
+++ src/components/CratingDetail.vue
@@ -15,8 +15,10 @@ const cargoData = computed(() => {
 
     const cargo = { ...item }
 
-    // 混装箱数和重量
+    // 混装箱数
     cargo.mixedBoxCount = cargo.mixedLayerCount * cargo.layerBoxCount
+
+    // 混装重量
     cargo.mixedNetWeight = cargo.mixedBoxCount * cargo.netWeight
     cargo.mixedGrossWeight = cargo.mixedBoxCount * cargo.grossWeight
 
@@ -29,16 +31,86 @@ const cargoData = computed(() => {
     cargo.freeWidth = truckWidth - cargo.relativeWidth * cargo.rowBoxCount
     cargo.freeHeight = truckHeight - cargo.relativeHeight * cargo.columnBoxCount
 
-    // 每排净重和毛重
-    cargo.layerNetWeight = cargo.netWeight * cargo.mixedBoxCount
-    cargo.layerGrossWeight = cargo.grossWeight * cargo.mixedBoxCount
+    // 每排重量
+    cargo.layerNetWeight = cargo.netWeight * cargo.layerBoxCount
+    cargo.layerGrossWeight = cargo.grossWeight * cargo.layerBoxCount
 
-    // 最大净重和毛重
+    // 最大重量
     cargo.maxNetWeight = cargo.netWeight * cargo.maxBoxCount
     cargo.maxGrossWeight = cargo.grossWeight * cargo.maxBoxCount
 
     return { ...cargo }
   })
+})
+
+const truckData = computed(() => {
+  if (!props.truck) return []
+
+  const netWeightTotal = props.truck.cargo.reduce((sum, item) => {
+    return sum + item.netWeight * item.mixedLayerCount * item.layerBoxCount
+  }, 0)
+
+  const grossWeightTotal = props.truck.cargo.reduce((sum, item) => {
+    return sum + item.grossWeight * item.mixedLayerCount * item.layerBoxCount
+  }, 0)
+
+  const freeLength =
+    props.truck.length -
+    props.truck.cargo.reduce((sum, item) => {
+      return sum + item.relativeLength * item.mixedLayerCount
+    }, 0)
+
+  const truckVolume =
+    props.truck.length * props.truck.width * props.truck.height
+
+  const cargoVolume = props.truck.cargo.reduce((sum, item) => {
+    const volume =
+      item.relativeLength * item.relativeWidth * item.relativeHeight
+    return sum + volume * item.mixedLayerCount * item.layerBoxCount
+  }, 0)
+
+  return [
+    {
+      label: '货车名称',
+      value: props.truck.name,
+    },
+    {
+      label: '剩余长度',
+      value: `${freeLength.toFixed(1)}m`,
+    },
+    {
+      label: '货车长度',
+      value: `${props.truck.length}mm`,
+    },
+    {
+      label: '货车宽度',
+      value: `${props.truck.width}mm`,
+    },
+    {
+      label: '货车高度',
+      value: `${props.truck.height}mm`,
+    },
+    {
+      label: '货车体积',
+      value: `${(truckVolume / 1000000000).toFixed(2)}m³`,
+    },
+    {
+      label: '货物体积',
+      value: `${(cargoVolume / 1000000000).toFixed(2)}m³`,
+    },
+    {
+      label: '使用率',
+      value: `${((cargoVolume / truckVolume) * 100).toFixed(2)}%`,
+    },
+    {
+      label: '货物净重',
+      value: `${netWeightTotal.toFixed(1)}kg`,
+    },
+    {
+      label: '货物毛重',
+      value: `${grossWeightTotal.toFixed(1)}kg`,
+    },
+  ]
 })
 
 const columns = [
@@ -171,6 +243,13 @@ const columns = [
 
 <template>
   <ACard title="装箱明细">
+    <ADescriptions
+      :data="truckData"
+      bordered
+      :column="{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }"
+      class="mb-4"
+      size="large"
+    ></ADescriptions>
     <ATable
       :columns
       :data="cargoData"
